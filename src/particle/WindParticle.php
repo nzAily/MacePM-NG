42445d543486d7d5cc27b8bdaa8cf2872695aa86
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-29 15:49:16 +0700

    Update WindParticle.php

--- src/particle/WindParticle.php
+++ src/particle/WindParticle.php
@@ -4,14 +4,15 @@ namespace XeonCh\Mace\particle;
 
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
-use pocketmine\network\mcpe\protocol\types\ParticleIds;
 use pocketmine\world\particle\Particle;
 
 class WindParticle implements Particle
 {
 
+    public const WIND_EXPLOSION = 91;
+
     public function encode(Vector3 $pos): array
     {
-        return [LevelEventPacket::standardParticle(ParticleIds::WIND_EXPLOSION, 0, $pos)];
+        return [LevelEventPacket::standardParticle(self::WIND_EXPLOSION, 0, $pos)];
     }
 }
